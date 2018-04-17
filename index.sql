adb70636b23793e144a19d648730833b6161ee8d
kingdome60 <38466997+kingdome60@users.noreply.github.com>
2018-04-17 21:20:51 +0700

    Add files via upload

---
+++ index.sql
@@ -0,0 +1,60 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+<script type="text/javascript">
+function MM_preloadImages() { //v3.0
+  var d=document; if(d.images){ if(!d.MM_p) d.MM_p=new Array();
+    var i,j=d.MM_p.length,a=MM_preloadImages.arguments; for(i=0; i<a.length; i++)
+    if (a[i].indexOf("#")!=0){ d.MM_p[j]=new Image; d.MM_p[j++].src=a[i];}}
+}
+function MM_swapImgRestore() { //v3.0
+  var i,x,a=document.MM_sr; for(i=0;a&&i<a.length&&(x=a[i])&&x.oSrc;i++) x.src=x.oSrc;
+}
+function MM_findObj(n, d) { //v4.01
+  var p,i,x;  if(!d) d=document; if((p=n.indexOf("?"))>0&&parent.frames.length) {
+    d=parent.frames[n.substring(p+1)].document; n=n.substring(0,p);}
+  if(!(x=d[n])&&d.all) x=d.all[n]; for (i=0;!x&&i<d.forms.length;i++) x=d.forms[i][n];
+  for(i=0;!x&&d.layers&&i<d.layers.length;i++) x=MM_findObj(n,d.layers[i].document);
+  if(!x && d.getElementById) x=d.getElementById(n); return x;
+}
+
+function MM_swapImage() { //v3.0
+  var i,j=0,x,a=MM_swapImage.arguments; document.MM_sr=new Array; for(i=0;i<(a.length-2);i+=3)
+   if ((x=MM_findObj(a[i]))!=null){document.MM_sr[j++]=x; if(!x.oSrc) x.oSrc=x.src; x.src=a[i+2];}
+}
+</script>
+<style type="text/css">
+body {
+	background-image: url(%E0%B8%A3%E0%B8%B9%E0%B8%9B/chalkboard-1337809_960_720.jpg);
+}
+</style>
+</head>
+
+<body onload="MM_preloadImages('รูป/2.jpg','รูป/4.jpg','รูป/6.jpg','รูป/00.jpg','รูป/8.jpg')">
+<table width="980" border="1" align="center">
+  <tr>
+    <td height="241"><img src="รูป/งาน.jpg" width="980" height="450" /></td>
+  </tr>
+  <tr>
+    <td height="60" bgcolor="#CCCCCC"><table width="200" border="1" align="center">
+      <tr>
+        <td height="50" bgcolor="#FFFFFF"><a href="#" onmouseout="MM_swapImgRestore()" onmouseover="MM_swapImage('Image6','','รูป/00.jpg',1)"><img src="รูป/0.jpg" width="196" height="50" id="Image6" /></a></td>
+      </tr>
+    </table></td>
+  </tr>
+  <tr>
+    <td height="63"><table width="982" height="48" border="1">
+      <tr>
+        <td width="236"><a href="งานสำเร็จ Vocabulary part.pptx" onmouseout="MM_swapImgRestore()" onmouseover="MM_swapImage('Image2','','รูป/2.jpg',1)"><img src="รูป/1.jpg" width="236" height="48" id="Image2" /></a></td>
+        <td width="245"><a href="งานสำเร็จ Grammar.pptx" onmouseout="MM_swapImgRestore()" onmouseover="MM_swapImage('Image7','','รูป/8.jpg',1)"><img src="รูป/7.jpg" width="227" height="51" id="Image7" /></a></td>
+        <td width="248"><a href="#" onmouseout="MM_swapImgRestore()" onmouseover="MM_swapImage('Image3','','รูป/4.jpg',1)"><img src="รูป/3.jpg" width="246" height="47" id="Image3" /></a></td>
+        <td width="225"><a href="สนทนา.mp4" onmouseout="MM_swapImgRestore()" onmouseover="MM_swapImage('Image4','','รูป/6.jpg',1)"><img src="รูป/5.jpg" width="246" height="49" id="Image4" /></a></td>
+      </tr>
+    </table></td>
+  </tr>
+</table>
+<embed src="y2mate.com - maggie_lindemann_pretty_girl_cheat_codes_x_cade_remix_official_video_qFmCXBL_4n8.mp3" width="91" height="46"></embed>
+</body>
+</html>
